d5a29838d031a6bdf8f3468d05677987e948b33f
liuk1221 <liuk.qwerty@gmail.com>
2023-09-06 10:47:04 +0200

    AssetHomepage + FormatMods

--- ProgettoAngular/src/app/componenti/nav-bar/nav-bar.component.ts
+++ ProgettoAngular/src/app/componenti/nav-bar/nav-bar.component.ts
@@ -6,11 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
-  constructor() {
-  }
-  
-
-
-
-
+  constructor() {}
+ 
 }
