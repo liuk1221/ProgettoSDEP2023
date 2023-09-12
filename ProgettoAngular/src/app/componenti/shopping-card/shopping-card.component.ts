a7a3d944713873c504afd258cff818acc5b67152
liuk1221 <liuk.qwerty@gmail.com>
2023-09-12 19:45:44 +0200

    MarmoJPG // ShoppingCard // NavBarFix

--- ProgettoAngular/src/app/componenti/shopping-card/shopping-card.component.ts
+++ ProgettoAngular/src/app/componenti/shopping-card/shopping-card.component.ts
@@ -1,22 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ForumDataDB } from "../../servizi/forum-data-db";
+import { FirebaseService } from 'src/app/servizi/firebase.service';
+
 
 @Component({
   selector: 'app-shopping-card',
   templateUrl: './shopping-card.component.html',
   styleUrls: ['./shopping-card.component.scss'],
 })
-export class ShoppingCardComponent {
+export class ShoppingCardComponent implements OnInit{
 
-  public desrizioni = {
-    'Travertino': 'Travertino',
-    'Marmo di Carrara': 'Il marmo Statuario di Carrara è una pietra naturale pregiata apprezzata per la sua eleganza, il colore bianco puro e le venature uniche. È una scelta popolare per progetti di design e architettura di alta qualità in tutto il mondo.',
-    'Calacatta' : 'Calacatta',
-    'Lasa': 'Lasa',
-    'Rosso di Verona': 'Rosso di Verona',
-    'Rosso di Levanto': 'Rosso di Levanto',
-    'Breccia Medicea': 'Braccia Medicea',
-    'Marmo di Siena': 'Marmo di Siena'
+
+  public rows : number = 4;
+  public prodotti : ForumDataDB[];
+  public marbleform: ForumDataDB = {
+    nome: '',
+    provenienza: '',
+    colore: '',
+    venature: false,
+    colore_v: '',
+    dim_x: 0,
+    dim_y: 0,
+    dim_z: 0,
+    qta: 0,
+    prezzo: 0
+  };
+  public pick : number[] = [0,0,0,0];
+  private ids : any[] = [];
+  private id_to_act : number = 0;
+
+  constructor(private firebase: FirebaseService) { }
+  
+  ngOnInit(): void {
+    this.four_random_cards()
+  }
+
+
+  private rand(range: number){
+    return Math.floor(Math.random() * range)
+  }
+
+  four_random_cards() {
+    this.firebase.retrieveMarble(
+      ).subscribe((data: any) => {
+        console.log(data)
+        this.prodotti = Object.keys(data).map(key => { 
+        this.ids.push(key)
+        data[key]['id'] = key
+        return data[key] 
+      })
+    });
+    let i = 0;
+    if (this.prodotti.length > 4) {      
+      while (i < 4 && this.pick[3] == 0) {
+        this.pick[i] = this.rand(this.prodotti.length)
+        for (let index = i; index > 0; index--) {
+          if (this.pick[i] == this.pick[index]) {
+            this.pick[i] = this.rand(this.prodotti.length)
+          } else {
+            break;
+          }
+        }
+        i++;
+      }
+    } else {
+      this.pick[0] = this.rand(this.prodotti.length);
+      this.pick[1] = this.rand(this.prodotti.length);
+      this.pick[2] = this.rand(this.prodotti.length);
+      this.pick[3] = this.rand(this.prodotti.length);
+    }
   }
 
 }
